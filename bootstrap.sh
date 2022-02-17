c0c29501faef63df03e3166f4d5eee4a5c74e4eb
Dušan Panić <dpanic@gmail.com>
2022-02-17 13:26:36 +0100

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sum=($(md5sum data/swat_2015.zip))
 
-if [[ "$sum" != "f417d68eeb219e8a02d19a25c08cf262" ]]; then
+if [[ "$sum" != "2fe90914b63b3843851d4c81e3fb892d" ]]; then
     wget -c "https://www.dropbox.com/s/iap6bx5qmklo1w4/swat_2015.zip?dl=0" -O data/swat_2015.zip
 else
     echo "File already downloaded! Skipping download"
